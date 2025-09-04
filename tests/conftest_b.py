4e46fc2e1512142e176f353e051830cde7acd188
jacktierney-dkit <d00279080@student.dkit.ie>
2025-09-04 10:30:41 +0100

    Update conftest_b.py

--- tests/conftest_b.py
+++ tests/conftest_b.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope="session")
 def driver():
     options = Options()
-    options.add_argument("--headless=new")   # run without opening a window
+    options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
