7078b2ad92b33769313b258b5dcbc355d6f9ff24
jacktierney-dkit <d00279080@student.dkit.ie>
2025-09-04 11:14:29 +0100

    Update test_home_page.py

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.by import By
 
 BASE_URL = "http://127.0.0.1:5000"
 
-@pytest.mark.usefixtures("driver")
 def test_home_page_heading(driver):
-    driver.get(BASE_URL + "/")
-    heading = driver.find_element(By.TAG_NAME, "h1").text
-    assert "Order" in heading or "Orders" in heading
+    driver.get(BASE_URL + "/get_orders")
+    assert "Orders" in driver.title
